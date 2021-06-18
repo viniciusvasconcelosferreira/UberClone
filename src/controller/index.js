0fc075e042a137125e5fd3c0fc4c9762f2433437
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-06-18 17:35:26 -0300

    Integração com o mercado pago

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const mercadopago = require('mercadopago');
 const cors = require('cors');
+const config = require('../config')
 
 let app = express();
 app.use(cors());
@@ -11,10 +12,40 @@ app.use(bodyParser.urlencoded({extended: false}));
 //REQUISIÇÕES JSON
 app.use(bodyParser.json());
 
+//CONFIGURAÇÃO DO MERCADO PAGO
+mercadopago.configure({
+    access_token: config.token
+});
+
 //ROUTES
 app.post('/', (req, res) => {
+    let preference = {
+        //ARRAY DE ITENS
+        items: [{
+            title: 'Blue shirt',
+            quantity: 10,
+            currency_id: 'BRL',
+            unit_price: 10
+        }],
+        payer: {
+            //EMAIL DO PAGADOR
+            email: "demo@mail.com"
+        },
+        payment_method: {
+            //LIMITE DE PARCELAS
+            installments: 3
+        }
+    };
+
+    mercadopago.preferences.create(preference).then(function (data) {
+        //RESPONSE PARA O FRONTEND
+        res.send(JSON.stringify(data.response.sandbox_init_point));
+    }).catch(function (error) {
+        console.log(error);
+    });
+
     // console.log(req.body);
-    res.send(JSON.stringify(`Com o valor de ${req.body.price} você consegue comprar várias coisas`));
+    // res.send(JSON.stringify(`Com o valor de ${req.body.price} você consegue comprar várias coisas`));
 })
 
 //START SERVER
--- src/views/Checkout/index.js
+++ src/views/Checkout/index.js
@@ -1,19 +1,21 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {LogBox, Text, View, TouchableOpacity} from 'react-native';
 import {css} from "./styles";
+import config from '../../config'
 // RECEBENDO OBJETO PROPS CRIADO PELO REACT NAVIGATION
 export default function Checkout(props) {
 
     useEffect(() => {
         async function sendServer() {
-            let response = await fetch('http://10.0.0.149:3000/', {
+            let response = await fetch(config.urlRoot, {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    price: 100
+                    price: props.route.params.price,
+                    address: props.route.params.address
                 })
             });
             let json = await response.json();
